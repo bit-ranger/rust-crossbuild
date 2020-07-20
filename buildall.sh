bae366b370b80498d19294a5689a68d91f181463
bit-ranger <sincerebravefight@gmail.com>
2020-07-20 11:47:33 +0800

    build release

--- buildall.sh
+++ buildall.sh
@@ -7,11 +7,11 @@ docker build -t rust-crossbuild .
 # Use -u so the resulting binaries are owned by the host user
 
 # linux
-docker run --rm -u $(id -u):$(id -g) -v $(pwd):/workdir rust-crossbuild /usr/local/rust/bin/cargo build
+docker run --rm -u $(id -u):$(id -g) -v $(pwd):/workdir rust-crossbuild /usr/local/rust/bin/cargo build --release
 
 # windows
-docker run --rm -u $(id -u):$(id -g) -v $(pwd):/workdir -e CROSS_TRIPLE=x86_64-w64-mingw32 rust-crossbuild /usr/local/rust/bin/cargo build --target=x86_64-pc-windows-gnu
+docker run --rm -u $(id -u):$(id -g) -v $(pwd):/workdir -e CROSS_TRIPLE=x86_64-w64-mingw32 rust-crossbuild /usr/local/rust/bin/cargo build --target=x86_64-pc-windows-gnu  --release
 
 # macos
-docker run --rm -u $(id -u):$(id -g) -v $(pwd):/workdir -e RUSTFLAGS="-C linker=/workdir/macbuild.sh" -e CROSS_TRIPLE=x86_64-apple-darwin rust-crossbuild /usr/local/rust/bin/cargo build --target=x86_64-apple-darwin
+docker run --rm -u $(id -u):$(id -g) -v $(pwd):/workdir -e RUSTFLAGS="-C linker=/workdir/macbuild.sh" -e CROSS_TRIPLE=x86_64-apple-darwin rust-crossbuild /usr/local/rust/bin/cargo build --target=x86_64-apple-darwin  --release
 
