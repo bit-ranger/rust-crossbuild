fb799a6d75be19526637ffc91d9f1be3fd779798
bit-ranger <sincerebravefight@gmail.com>
2021-06-18 16:11:36 +0800

    feat(rust-crossbuild): script

--- macos_build.sh
+++ macos_build.sh
@@ -3,5 +3,5 @@
 set -e
 
 # macos
-docker run --rm -u "$(id -u)":"$(id -g)" -v "$(pwd)":/workdir -e RUSTFLAGS="-C linker=/usr/local/macos/macos_linker.sh" -e CROSS_TRIPLE=x86_64-apple-darwin rust-crossbuild /usr/local/cargo/bin/cargo build --target=x86_64-apple-darwin  --release --verbose
+docker run --rm  --cpus="0.5" -v "$(pwd)":/workdir -e RUSTFLAGS="-C linker=/usr/local/macos/macos_linker.sh" -e CROSS_TRIPLE=x86_64-apple-darwin rust-crossbuild /usr/local/cargo/bin/cargo build --target=x86_64-apple-darwin  --release --verbose
 
--- windows_build.sh
+++ windows_build.sh
@@ -4,5 +4,5 @@ set -e
 
 
 # windows
-docker run --rm -u "$(id -u)":"$(id -g)" -v "$(pwd)":/workdir -e CROSS_TRIPLE=x86_64-w64-mingw32 rust-crossbuild /usr/local/cargo/bin/cargo build --target=x86_64-pc-windows-gnu  --release --verbose
+docker run --rm  --cpus="0.5" -v "$(pwd)":/workdir -e CROSS_TRIPLE=x86_64-w64-mingw32 rust-crossbuild /usr/local/cargo/bin/cargo build --target=x86_64-pc-windows-gnu  --release --verbose
 
