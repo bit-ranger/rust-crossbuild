aaedb0b8c080a8e74f709dcf0dbec650b848a3ab
bit-ranger <sincerebravefight@gmail.com>
2021-06-18 18:58:11 +0800

    feat(rust-cross): script

--- linux_build.sh
+++ linux_build.sh
@@ -2,4 +2,4 @@
 
 set -e
 # linux
-docker run --rm --cpus="0.5"  -v "$(pwd)":/workdir rust-crossbuild /usr/local/cargo/bin/cargo build --release --verbose
+docker run --rm --cpus="0.5"  -v "$(pwd)":/workdir rust-cross /usr/local/cargo/bin/cargo build --release --verbose
--- macos_build.sh
+++ macos_build.sh
@@ -3,5 +3,5 @@
 set -e
 
 # macos
-docker run --rm  --cpus="0.5" -v "$(pwd)":/workdir -e RUSTFLAGS="-C linker=/usr/local/macos/macos_linker.sh" -e CROSS_TRIPLE=x86_64-apple-darwin rust-crossbuild /usr/local/cargo/bin/cargo build --target=x86_64-apple-darwin  --release --verbose
+docker run --rm  --cpus="0.5" -v "$(pwd)":/workdir -e RUSTFLAGS="-C linker=/usr/local/macos/macos_linker.sh" -e CROSS_TRIPLE=x86_64-apple-darwin rust-cross /usr/local/cargo/bin/cargo build --target=x86_64-apple-darwin  --release --verbose
 
